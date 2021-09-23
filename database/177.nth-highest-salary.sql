50a6151dedc19876a792451f2cfd30339d4083d4
Paris Zhang <paris.zhang@masimo.com>
2021-09-23 13:15:21 -0700

    Database 177 - add one MySQL solution.

--- database/177.nth-highest-salary.sql
+++ database/177.nth-highest-salary.sql
@@ -1,15 +1,28 @@
 /* [177] Nth Highest Salary */
 
-
--- MySQL
+-- MySQL, Solution 1
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
 DECLARE M INT;
-SET M=N-1;
+SET M=N-1; -- No calculation can happen in the argument
   RETURN (
       # Write your MySQL query statement below.
       SELECT DISTINCT Salary FROM Employee ORDER BY Salary DESC LIMIT M, 1
   );
+END
+
+-- MySQL, Solution 2
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+RETURN (
+  select distinct e1.salary
+  from Employee e1
+  where N-1 = (
+    select count(distinct e2.Salary)
+    from Employee e2
+    where e1.Salary < e2.Salary
+  )
+);
 END
 
 
