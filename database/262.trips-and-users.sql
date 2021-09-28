ee4cfba31098f643623f9a3799818e14c61b5b74
Paris Zhang <paris.zhang@masimo.com>
2021-09-27 19:13:11 -0700

    Add database 262. trips and users. Add 3 mysql solutions with runtime comparison.

---
+++ database/262.trips-and-users.sql
@@ -0,0 +1,57 @@
+/* [262] Trips and Users */
+
+-- 389 ms (96.6%)
+with cte as (
+    select Users_Id from Users where Banned = 'No'
+)
+
+select
+  Request_at as Day,
+  round(sum(case when Status != 'completed' then 1 else 0 end) / count(Id), 2) as `Cancellation Rate`
+from Trips t
+where (Request_at between date '2013-10-01' and date '2013-10-03')
+  and Client_Id in (select Users_Id from cte)
+  and Driver_Id in (select Users_Id from cte)
+group by Request_at
+
+-- 452 ms (64%)
+with cte as (
+    select Users_Id from Users where Banned = 'No'
+)
+
+select
+  Request_at as Day,
+  round(count(case when Status != 'completed' then 1 end) / count(Id), 2) as `Cancellation Rate`
+from Trips t
+where (Request_at between date '2013-10-01' and date '2013-10-03')
+  and Client_Id in (select Users_Id from cte)
+  and Driver_Id in (select Users_Id from cte)
+group by Request_at
+
+
+-- 519 ms (36%)
+select
+  Request_at as Day,
+  round(count(case when Status != 'completed' then 1 end) / count(Id), 2) as `Cancellation Rate`
+from Trips t
+where (Request_at between date '2013-10-01' and date '2013-10-03')
+  and Client_Id in (select Users_Id from Users where Banned = 'No')
+  and Driver_Id in (select Users_Id from Users where Banned = 'No')
+group by Request_at-- @lc code=start
+# Write your MySQL query statement below
+
+with cte as (
+    select Users_Id from Users where Banned = 'No'
+)
+
+select
+  Request_at as Day,
+  round(sum(case when Status != 'completed' then 1 else 0 end) / count(Id), 2) as `Cancellation Rate`
+from Trips t
+where (Request_at between date '2013-10-01' and date '2013-10-03')
+  and Client_Id in (select Users_Id from cte)
+  and Driver_Id in (select Users_Id from cte)
+group by Request_at
+
+-- @lc code=end
+
