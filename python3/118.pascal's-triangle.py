f8d1982a4a71e98419385e70763f0e13df0f4aa8
Paris Zhang <parissyzhang@gmail.com>
2021-11-08 18:19:37 -0800

    DS 118. Pascal's triangle - adding my brute force solution, optimized version, and a top down recursive

---
+++ python3/118.pascal's-triangle.py
@@ -0,0 +1,42 @@
+# Data Structure Study Plan 1
+# Day 4
+# 118. Pascal's triangle (Easy)
+
+from typing import List
+
+# My brute force solution (Runtime 28 ms, 88%; Memory 14.1 MB, 94%)
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        # My brute force solution
+        out = [[1]] # Initiate the initial list        
+        for i in range(1, numRows): # Loop through each row            
+            seq = [0] * (i+1) # Initiate the list for row i            
+            seq[0] = seq[i] = 1 # Initiate the head and tail entries           
+            for j in range(1,i): # Calculate the sum of two numbers
+                seq[j] = out[i-1][j-1] + out[i-1][j]
+            out.append(seq)
+        return out
+
+# Solution 2, optimal - Dynamic programming, iterative (Runtime 20 ms, 99.5%; Memory 14.2 MB, 82%)
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        pascal = [[1]*i for i in range(1, numRows+1)] # Initialize triangle with all 1s
+        for i in range(1, numRows):
+            for j in range(1, i):
+                pascal[i][j] = pascal[i-1][j] + pascal[i-1][j-1]
+        return pascal
+## Time = O(n^2)
+## Space = O(n^2)
+
+# Solution 3 - top down recursive (Runtime 32 ms, 71%; Memory 14.3 MB, 24%)
+class Solution:
+    def generate(self, n: int) -> List[List[int]]:
+        def helper(n):
+            if n:
+                helper(n-1)             # generate above row first
+                pascal.append([1]*n)    # insert current row into triangle
+                for i in range(1, n-1): # update current row values using above row
+                    pascal[n-1][i] = pascal[n-2][i] + pascal[n-2][i-1]
+        pascal = []
+        helper(n)
+        return pascal
