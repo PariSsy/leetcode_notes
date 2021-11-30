a58ba3be759b9e036642c66934735f365c2f7e61
Paris <parissyzhang@gmail.com>
2021-11-30 15:15:55 -0800

    Algo 704. Binary search - add my solution in rev 1; update binary search time and space complexity

--- python3/704.binary-search.py
+++ python3/704.binary-search.py
@@ -1,9 +1,26 @@
 # Algorithm 1 Study Plan
 # Day 1
-# 704. Binary Search
+# 704. Binary Search (Easy)
+
+from typing import List
+
+# (rev 1) my solution, binary search - 232 ms, 85%; 15.7 mb, 31%
+## See solution 1 for another writing
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        out = -1
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            travel = left + (right - left) // 2
+            if nums[travel] == target:
+                return travel
+            elif nums[travel] > target:
+                right = travel - 1
+            else:
+                left = travel + 1
+        return out
 
 # Solution 1 (Runtime = 376 ms)
-from typing import List
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         left, right = 0, len(nums) - 1
@@ -16,6 +33,8 @@ class Solution:
             else:
                 left = pivot + 1
         return -1
+## Time = O(log N)
+## Space = O(1)
         
 # Solution 2 (Runtime = 292 ms)
 import bisect
