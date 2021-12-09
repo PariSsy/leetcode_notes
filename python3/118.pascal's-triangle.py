629e3eca868a1ddfb712b714f0dd0e9d2758b1fb
Paris <parissyzhang@gmail.com>
2021-12-09 14:24:38 -0800

    DS 118. pascal's triangle - add rev 1 solution

--- python3/118.pascal's-triangle.py
+++ python3/118.pascal's-triangle.py
@@ -4,11 +4,28 @@
 
 from typing import List
 
-# My brute force solution (Runtime 28 ms, 88%; Memory 14.1 MB, 94%)
+# (rev 1) (24 ms, 97%; 14.2 mb, 83%)
+class Solution:
+    def generate(self, n: int) -> List[List[int]]:
+        # (rev 1)
+        out = [[1]] # Initiate the first row
+        num = 1 # Initiate index for the second rwo
+        while num < n:
+            prev = out[num - 1] # Retrieve previous row
+            new_middle = [] # Create empty array to hold values for the new row
+            for i in range(len(prev) - 1): # Loop through the 1st to the 2nd last index of the previous row
+                new_middle.append(prev[i] + prev[i+1]) # Add sum to the middle of the new row
+            new = [1] + new_middle + [1] # Add boundaries to the new row
+            out.append(new) # Append new row to final output
+            num += 1 # Go to next row
+        return out
+
+
+# (new) My brute force solution (Runtime 28 ms, 88%; Memory 14.1 MB, 94%)
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         # My brute force solution
-        out = [[1]] # Initiate the initial list        
+        out = [[1]] # Initiate the initial list
         for i in range(1, numRows): # Loop through each row            
             seq = [0] * (i+1) # Initiate the list for row i            
             seq[0] = seq[i] = 1 # Initiate the head and tail entries           
@@ -16,6 +33,18 @@ class Solution:
                 seq[j] = out[i-1][j-1] + out[i-1][j]
             out.append(seq)
         return out
+
+# (new) my brute force improved (24 ms, 97%; 14 mb, 99.4%) 
+class Solution:
+    def generate(self, n: int) -> List[List[int]]:       
+        out = [[1]] # Initiate the initial list
+        for i in range(1, n): # Loop through each row            
+            seq = [1] * (i+1) # Initiate the list for row i           
+            for j in range(1,i): 
+                seq[j] = out[i-1][j-1] + out[i-1][j] # Calculate the sum of two numbers
+            out.append(seq)
+        return out
+
 
 # Solution 2, optimal - Dynamic programming, iterative (Runtime 20 ms, 99.5%; Memory 14.2 MB, 82%)
 class Solution:
@@ -27,6 +56,7 @@ class Solution:
         return pascal
 ## Time = O(n^2)
 ## Space = O(n^2)
+
 
 # Solution 3 - top down recursive (Runtime 32 ms, 71%; Memory 14.3 MB, 24%)
 class Solution:
