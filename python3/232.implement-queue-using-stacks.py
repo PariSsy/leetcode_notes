38789845c32f78798c6cc9da6d5b1ee0774f7bab
Paris <parissyzhang@gmail.com>
2021-12-13 15:28:21 -0800

    DS 232. Implement queue using stacks - add 2 python implementation from discussion

---
+++ python3/232.implement-queue-using-stacks.py
@@ -0,0 +1,60 @@
+# Data Structure Study Plan I
+# Day 9
+# 232. Implement queue using stacks (Easy)
+
+# Solution Python 3 implementation (by sfdye)
+
+# Discussion 2, push O(1), pop O(n) (32 ms, 58%)
+class MyQueue:
+
+    def __init__(self):
+        self.s1 = []
+        self.s2 = []
+
+    def push(self, x: int) -> None:
+        self.s1.append(x)
+
+    def pop(self) -> int:
+        self.peek()
+        return self.s2.pop()
+
+    def peek(self) -> int:
+        if not self.s2:
+            while self.s1:
+                self.s2.append(self.s1.pop())
+        return self.s2[-1]
+
+    def empty(self) -> bool:
+        return not self.s1 and not self.s2
+
+
+# Discussion 1, push O(n), pop O(1)
+class MyQueue:
+
+    def __init__(self):
+        self.s1 = []
+        self.s2 = []
+
+    def push(self, x: int) -> None:
+        while self.s1:
+            self.s2.append(self.s1.pop())
+        self.s1.append(x)
+        while self.s2:
+            self.s1.append(self.s2.pop())
+
+    def pop(self) -> int:
+        return self.s1.pop()
+
+    def peek(self) -> int:
+        return self.s1[-1]
+
+    def empty(self) -> bool:
+        return not self.s1
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
