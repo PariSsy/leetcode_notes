2b82a77aeb2388830a631aaf7fdf4767f934323a
Paris Zhang <parissyzhang@gmail.com>
2021-12-28 14:43:46 -0800

    977. Squares of a sorted array - add rev 2 solution, two pointers.

--- python3/977.squares-of-a-sorted-array.py
+++ python3/977.squares-of-a-sorted-array.py
@@ -2,6 +2,23 @@
 # Day 2
 # 977. Squares of a sorted array
 
+# (rev 2) two pointers (232 ms, 59%; 16.2 mb, 31%)
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        out = [0] * n
+        left, right = 0, n-1
+        while left <= right:
+            sq_left = nums[left] * nums[left]
+            sq_right = nums[right] * nums[right]
+            if sq_left <= sq_right:
+                out[right - left] = sq_right
+                right -= 1
+            else:
+                out[right - left] = sq_left
+                left += 1
+        return out
+
 # Solution 1 - built-in function (300 ms)
 from typing import List
 
