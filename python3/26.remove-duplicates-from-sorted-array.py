ad2cba2fdf70c77360b072d1018c5282b469ce4c
Paris <parissyzhang@gmail.com>
2022-01-18 14:25:09 -0800

    26. remove duplicates from sorted array - add rev 1 solution, two pointers

--- python3/26.remove-duplicates-from-sorted-array.py
+++ python3/26.remove-duplicates-from-sorted-array.py
@@ -1,8 +1,24 @@
 # Explore - Intro to Data Structure
 # Arrays 101
-# 28. Remove duplicates from sorted array (Easy)
+# 26. Remove duplicates from sorted array (Easy)
 
-# solution discussion by zhengzhicong (88 ms, 70%)
+
+# rev 1, two pointers (163 ms, 19%)
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if len(nums) <= 1:
+            return len(nums)
+        keeper = mover = 0
+        while mover < len(nums):
+            if nums[keeper] < nums[mover]:
+                keeper += 1
+                nums[keeper] = nums[mover]
+            else:
+                mover += 1              
+        return keeper+1
+
+
+# solution discussion by zhengzhicong, one pointer (88 ms, 70%)
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         mover = 1
